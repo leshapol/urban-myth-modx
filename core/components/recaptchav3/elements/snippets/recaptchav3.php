1bc7ce6eac542558fd11a11cdbe854c37a2bb3fe
leshapol <leshapol@gmail.com>
2023-09-12 19:13:53 +0300

    recaptcha3, ajaxform add

---
+++ core/components/recaptchav3/elements/snippets/recaptchav3.php
@@ -0,0 +1,39 @@
+<?php
+$recaptcha = $hook->getValue('g-recaptcha-response');
+$url_google_api = "https://www.google.com/recaptcha/api/siteverify";
+$secret = $modx->getOption('formit.recaptcha_private_key');
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$modx->lexicon->load('recaptchav3:default');
+
+class Captcha{
+    public function getCaptcha($SecretKey, $secret, $url_google_api, $ip){
+        $response = file_get_contents($url_google_api . "?secret=" . $secret . "&response=" . $SecretKey . "&remoteip=" . $ip);
+        $data = json_decode($response);
+        return $data;
+    }
+}
+
+if ($recaptcha) {
+    $ObjCaptcha = new Captcha();
+    $data = $ObjCaptcha->getCaptcha($recaptcha, $secret, $url_google_api, $ip);
+
+    if ($data->success && $data->score >= 0.5) {
+        return true;
+    } else {
+        $hook->addError('g-recaptcha-response', $modx->lexicon('recaptchav3_check_error'));
+        $error_message = "";
+        $error_message .= $modx->lexicon('recaptchav3_check_error_log') . "<br/>";
+        foreach ($data->{'error-codes'} as $k => $v) {
+            $error_message .= "{$k} - {$v}<br/>";
+        }
+        $modx->log(MODX_LOG_LEVEL_ERROR, $error_message);
+        return false;
+    }
+} else {
+    $hook->addError('g-recaptcha-response', $modx->lexicon('recaptchav3_check_empty_error'));
+    $modx->log(MODX_LOG_LEVEL_ERROR, $modx->lexicon('recaptchav3_check_empty_error_log'));
+    return false;
+}
+
+return true;
---
+++ core/packages/recaptchav3-1.0.7-beta/modCategory/333db12c406a0c987b7df0e9b2cdc357/0/recaptchav3/elements/snippets/recaptchav3_html.php
@@ -0,0 +1,37 @@
+<?php
+$public = $modx->getOption('formit.recaptcha_public_key');
+$action = $action ?: 'ajaxform';
+
+if (!$modx->getPlaceholder('rcv3_initialized')) {
+    $modx->regClientStartupScript('<script src="https://www.google.com/recaptcha/api.js?onload=ReCaptchaCallbackV3&render=' . $public . '" async></script>');
+    $modx->regClientScript('
+        <script>
+            var ReCaptchaCallbackV3 = function() {
+                grecaptcha.ready(function() {
+                    grecaptcha.reset = grecaptchaExecute;
+                    grecaptcha.reset();
+                });
+            };
+            function grecaptchaExecute() {
+                grecaptcha.execute("' . $public . '", { action: "' . $action . '" }).then(function(token) {
+                    var fieldsToken = document.querySelectorAll("[name =\'g-recaptcha-response\']");
+                    Array.prototype.forEach.call(fieldsToken, function(el, i){
+                        el.value = token;
+                    });
+                });
+            };
+            // обновляем капчу каждую минуту
+            setInterval(function() {
+                grecaptcha.reset();
+            }, 60000);
+        </script>
+    ', true);
+    $modx->setPlaceholder('rcv3_initialized', 1);
+}
+
+$output = '
+    <span class="error_g-recaptcha-response error">' . $error . '</span>
+    <input type="hidden" name="g-recaptcha-response">
+';
+
+return $output;
